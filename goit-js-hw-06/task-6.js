ea394f5314db8544039d091c331d1d33cf54d610
Gennadiy <shelukhin_g@ukr.net>
2020-02-03 14:38:39 +0200

    homework

--- goit-js-hw-06/task-6.js
+++ goit-js-hw-06/task-6.js
@@ -2,7 +2,7 @@
 import users from "./users.js";
 
 const getUsersWithAge = (users, min, max) => {
-  return users.filter(user => user.age > min).filter(user => user.age < max);
+  return users.filter(user => user.age > min && user.age < max);
 };
 
 console.table(getUsersWithAge(users, 20, 30)); // [объект Ross Vazquez, объект Elma Head, объект Carey Barr]
