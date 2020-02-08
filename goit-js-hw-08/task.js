86a3576c2d2a544b4930eb73e018d0dc8eec7330
Gennadiy <shelukhin_g@ukr.net>
2020-02-09 00:20:14 +0200

    homework-8

---
+++ goit-js-hw-08/task.js
@@ -0,0 +1,34 @@
+"use strict";
+
+import galleryList from "./gallery-items.js";
+const elementUl = document.querySelector(".js-gallery");
+const elementDiv = document.querySelector(".lightbox");
+const elementImg = document.querySelector(".lightbox__image");
+const singleBtn = document.querySelector(".lightbox__button");
+const itemList = galleryList.reduce(
+  (acc, item) =>
+    acc +
+    `<li class="gallery__item"><a class="gallery__link" href="#"><img class="gallery__image" src =${item.preview} data-source=${item.original} alt=${item.description}></a></li>`,
+  ""
+);
+elementUl.insertAdjacentHTML("beforeend", itemList);
+
+elementUl.addEventListener("click", handleNavClick);
+singleBtn.addEventListener("click", handleClick);
+
+function handleNavClick(event) {
+  const target = event.target;
+  console.dir(event.target);
+  if (target.classList.contains("gallery__image")) {
+    elementDiv.classList.add("is-open");
+    elementImg.setAttribute("src", target.dataset.source);
+  }
+}
+
+function handleClick(event) {
+  const target = event.target;
+  if (target.classList.contains("lightbox__button")) {
+    elementImg.removeAttribute("src");
+    elementDiv.classList.remove("is-open");
+  }
+}
